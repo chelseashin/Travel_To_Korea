2262a70988892a9b4d66378fdfef820285f816fc
ChaewonShin <chaewonshin95@gmail.com>
2019-07-24 10:19:08 +0900

    header login modal 구현 중

--- maps/urls.py
+++ maps/urls.py
@@ -5,7 +5,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-# app_name = 'maps'
+app_name = 'maps'
 
 # schema_url_v1_patterns = [
 #     url(r'^v1/', include('maps.urls', namespace='map_api')),
@@ -38,5 +38,4 @@ urlpatterns = [
     path('detail/<int:content_id>/', views.detailpage),
     # path('docs/', get_swagger_view(title="여행 정보 API")),
     path('detail/', views.test),
-
     ]
